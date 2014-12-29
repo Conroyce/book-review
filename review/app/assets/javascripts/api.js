1a4316e941b8fb4127fed66245b67d8ef01b6084
Connor Kojimoto <cokojimoto@gmail.com>
2014-12-28 17:54:08 -0800

    search function works

--- review/app/assets/javascripts/api.js
+++ review/app/assets/javascripts/api.js
@@ -1,12 +1,20 @@
 window.API = (function() {
+  var api = "AIzaSyD1xQ8m_sQA8m2wDDP6iroUlHf2ei2ehuc";
+  var start = "https://www.googleapis.com/books/v1/volumes?q="
   var allBooks = "https://www.googleapis.com/books/v1/volumes?q=search+terms";
-
+  
 
   var getAll = function(callback) {
     $.get(allBooks,callback)
   };
 
+  var find = function(topic,callback) {
+    $.get(start+topic+"&key="+api,callback)
+  };
+
+
   return {
-    getAll: getAll
+    getAll: getAll,
+    find: find
   }
 })();
--- review/app/assets/javascripts/ui.js
+++ review/app/assets/javascripts/ui.js
@@ -1,3 +1,4 @@
+
 API.getAll(function(books) {
   console.log(books)
   var bookList = [];
@@ -5,10 +6,11 @@ API.getAll(function(books) {
     var ans = {};
 
     ans.title = book.volumeInfo.title;
-    ans.img = book.volumeInfo.thumbnail;
+    ans.img = book.volumeInfo.imageLinks.thumbnail;
     ans.authors = book.volumeInfo.authors;
+    ans.description = book.volumeInfo.description;
 
-    bookList.push(ans)
+    bookList.push(ans);
   });
 
   var source = $('#book-template').html();
@@ -17,3 +19,31 @@ API.getAll(function(books) {
 
   $('.main').append(html);
 });
+
+$(document).ready(function() {
+  $('.search-div').on('click','.searchButton',function() {
+    var $text = $('#searchBox').val();
+    console.log($text);
+    API.find($text,function(data) {
+      console.log(data);
+      var bookList = [];
+      data.items.forEach(function(book) {
+        var ans = {};
+
+        ans.title = book.volumeInfo.title;
+        ans.authors = book.volumeInfo.authors;
+        ans.img = book.volumeInfo.imageLinks.thumbnail
+        bookList.push(ans);
+      });
+
+      var source = $('#book-template').html();
+      var template = Handlebars.compile(source);
+      var html = template({books:bookList})
+     
+      $('.main').html("");
+      $('.main').append(html);
+    });
+  });
+});
+
+
