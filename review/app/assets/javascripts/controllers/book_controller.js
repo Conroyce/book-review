0aef1a6623923c3f485130c42917f1192e92058e
Connor Kojimoto <cokojimoto@gmail.com>
2015-01-22 14:59:16 -0800

    fixed issue with switching pages, prepping to display individual books

--- review/app/assets/javascripts/controllers/book_controller.js
+++ review/app/assets/javascripts/controllers/book_controller.js
@@ -3,7 +3,7 @@ app.controller("BookCtrl",["$http","$scope",function($http,$scope) {
   $scope.method = "GET";
   $scope.url = "https://www.googleapis.com/books/v1/volumes?q=Fiction&maxResults=18";
   
-  
+$scope.book = "";  
 $scope.code = null;
 $scope.reponse = null;
 
@@ -15,3 +15,7 @@ $http({method: $scope.method, url: $scope.url}).
 });
 
 }])
+
+$scope.getBook = function(num) {
+  $scope.book = 
+}
