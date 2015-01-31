ca3fb32ac6bce0c0e4490015fa1cae4f2f15c00f
Connor Kojimoto <cokojimoto@gmail.com>
2015-01-30 19:29:47 -0800

    checking for git sssh

--- review/app/assets/javascripts/controllers/book_controller.js
+++ review/app/assets/javascripts/controllers/book_controller.js
@@ -6,7 +6,6 @@ app.controller("BookCtrl",["$scope","BooksBook","Messages","GetMessages","$http"
   $scope.messages = $http.get("/books/"+$scope.book.id+"/messages").success(function(data) { 
     console.log(data); 
   });
-  // $scope.getMessage = GetMessages.query();
 
   $scope.createMessage = function() {
     var obj = {
@@ -20,8 +19,6 @@ app.controller("BookCtrl",["$scope","BooksBook","Messages","GetMessages","$http"
     }    
        
     $http(obj).success(function(data) {console.log(data);});
-    // Getmessages.save($scope.reviewTitle,$scope.review,$scope.book_id);
-    // Messages.create($scope.reviewTitle,$scope.review,$scope.book_id);
     $scope.reviewTitle="";
     $scope.review = "";
     
