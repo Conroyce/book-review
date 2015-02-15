b0506c14144fde5abfe6c841da087b6b31bf0b75
Connor Kojimoto <cokojimoto@gmail.com>
2015-02-14 19:32:48 -0800

    fixes issue with books not loading, removes unnecessary code

--- review/app/assets/javascripts/ui.js
+++ review/app/assets/javascripts/ui.js
@@ -1,3 +1,5 @@
+var booksObj = {};
+
 var paramsCheck = function() {
   var sPageURL = window.location.href.split('/');
   if (sPageURL[sPageURL.length-2] == "books") {
@@ -14,64 +16,51 @@ var paramsCheck = function() {
   }
 }
 
-$(document).ready(function() {  
+var getBooks = function(books) {
+  booksObj = {};
+  var bookList = [];
+  for (var i = 0; i < books.items.length; i++) {
+    var ans = {};
+    var bookItem = {};
+    var checkTitle = books.items[i].volumeInfo.title.split("");
 
-  var booksObj = {};
+    if (checkTitle.length > 28) {
+      ans.dispTitle = checkTitle.join("").substr(0,28)+"...";      
+    } else {
+      ans.dispTitle = books.items[i].volumeInfo.title;
+    }
+    
+    if (books.items[i].volumeInfo.imageLinks == undefined) {
+      ans.img = "/mysterybook.jpg";
+    } else {
+      ans.img = books.items[i].volumeInfo.imageLinks.thumbnail;
+    } 
 
-  var getBooks = function(books) {
-    booksObj = {};
-    var bookList = [];
-    var counter = 0;
-    for (var i = 0; i < books.items.length; i++) {
-      var ans = {};
-      var bookItem = {}
-      var checkTitle = books.items[i].volumeInfo.title.split("");
+    ans.authors = books.items[i].volumeInfo.authors;
+    ans.description = books.items[i].volumeInfo.description;
+    ans.id = books.items[i].id;
+    ans.el = i;
 
-      if (checkTitle.length > 28) {
-        ans.dispTitle = checkTitle.join("").substr(0,28)+"...";      
-      } else {
-        ans.dispTitle = books.items[i].volumeInfo.title;
-      }
-      
-      if (books.items[i].volumeInfo.imageLinks == undefined) {
-        ans.img = "/mysterybook.jpg";
-        bookItem.img = "/mysterybook.jpg";
-      } else {
-        ans.img = books.items[i].volumeInfo.imageLinks.thumbnail;
-        bookItem.img = books.items[i].volumeInfo.imageLinks.thumbnail;
-      } 
+    bookItem.book_id = books.items[i].id;
+    bookItem.title = books.items[i].volumeInfo.title;
+    booksObj[i] = bookItem;
 
-      ans.authors = books.items[i].volumeInfo.authors;
-      ans.description = books.items[i].volumeInfo.description;
-      ans.id = books.items[i].id;
-      ans.el = i;
-
-      bookItem.description = books.items[i].volumeInfo.description;
-      bookItem.book_id = books.items[i].id;
-      bookItem.title = books.items[i].volumeInfo.title;
-      bookItem.rating = books.items[i].volumeInfo.averageRating || "";
-      bookItem.ratingCount = books.items[i].volumeInfo.ratingsCount || "";
-      bookItem.link = books.items[i].accessInfo.webReaderLink;
-      booksObj[i] = bookItem;
-
-      bookList.push(ans);
-      counter++;
-    };
-
-    var source = $('#book-template').html();
-    var template = Handlebars.compile(source);
-    var html = template({books:bookList});
-
-    $('.main').html('')
-    $('.main').append(html);
+    bookList.push(ans);
   };
 
-  API.getAll(getBooks);
+  var source = $('#book-template').html();
+  var template = Handlebars.compile(source);
+  var html = template({books:bookList});
 
+  $('.main').html('')
+  $('.main').append(html);
+};
+
+$(document).ready(function() {  
   $('.main').on('click','.addBook',function(e) {
     var $el = $(this).children("input.bookEl").val();
     var $userId = $(this).children("input.userId").val();
-    var book = booksObj[$el];);
+    var book = booksObj[$el];
 
     $.ajax({
       type: "POST",
@@ -91,24 +80,10 @@ $(document).ready(function() {
   });
 
   $('.main').on('click','.show-book',function(e) {
-    // e.preventDefault();
     var $el = $(this).children().children("input.bookEl").val();
     var book = booksObj[$el];
 
     $.post("/books",{ book: { book_id: book.book_id } });
-
-    // $.post("/books", 
-    //   {
-    //     book:{
-    //       title: book.title, 
-    //       book_id: book.book_id, 
-    //       rating: book.rating,
-    //       ratingCount: book.ratingCount,
-    //       description: book.description,
-    //       link: book.link,
-    //       img: book.img
-    //     }  
-    //   });
   });
 
   $('.search-div').on('submit',function(e) {
@@ -138,12 +113,6 @@ $(document).ready(function() {
       }
     });
   });
-
-  $('.homeLink').on('click',function(e) {
-    console.log("hey");
-    API.getAll(getBooks)
-    // API.find();
-  })
 });
 
 
