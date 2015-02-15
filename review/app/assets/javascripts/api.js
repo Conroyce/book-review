f2a1c36510cc6aa27f3a3f75f28365fb65b245c0
Connor Kojimoto <cokojimoto@gmail.com>
2015-02-14 16:06:31 -0800

    can now view book data upon clicking a book

--- review/app/assets/javascripts/api.js
+++ review/app/assets/javascripts/api.js
@@ -1,7 +1,6 @@
 window.API = (function() {
   var start = "https://www.googleapis.com/books/v1/volumes"
   var allBooks = "https://www.googleapis.com/books/v1/volumes?q=Fiction&maxResults=18";
-  
 
   var getAll = function(callback) {
     $.get(allBooks,callback)
--- review/app/assets/javascripts/ui.js
+++ review/app/assets/javascripts/ui.js
@@ -1,62 +1,70 @@
-$(document).ready(function() {
+var paramsCheck = function() {
   var sPageURL = window.location.href.split('/');
-  console.log(sPageURL); 
   if (sPageURL[sPageURL.length-2] == "books") {
-    API.find(sPageURL[sPageURL.length-1], 
-      function(data) {
-        var book = data.items[0];
-        $('bookShowTitle').html(book.volumeInfo.title)
-      });
+  API.find(sPageURL[sPageURL.length-1], 
+    function(data) {
+      var book = data.items[0];
+      console.log("paramsCheck",book);
+      $('.bookShowTitle').html(book.volumeInfo.title);
+      $('.bookShowImg').html('<img src="' + book.volumeInfo.imageLinks.thumbnail + '">');
+      $('.bookShowRating').html('Average Rating: ' + book.volumeInfo.averageRating);
+    });
   }
+}
+
+$(document).ready(function() {  
+  var book;
+
+  paramsCheck();
 
   var booksObj = {};
 
   var getBooks = function(books) {
-      booksObj = {};
-      var bookList = [];
-      var counter = 0;
-      for (var i = 0; i < books.items.length; i++) {
-        var ans = {};
-        var bookItem = {}
-        var checkTitle = books.items[i].volumeInfo.title.split("");
+    booksObj = {};
+    var bookList = [];
+    var counter = 0;
+    for (var i = 0; i < books.items.length; i++) {
+      var ans = {};
+      var bookItem = {}
+      var checkTitle = books.items[i].volumeInfo.title.split("");
 
-        if (checkTitle.length > 28) {
-          ans.dispTitle = checkTitle.join("").substr(0,28)+"...";      
-        } else {
-          ans.dispTitle = books.items[i].volumeInfo.title;
-        }
-        
-        if (books.items[i].volumeInfo.imageLinks == undefined) {
-          ans.img = "/mysterybook.jpg";
-          bookItem.img = "/mysterybook.jpg";
-        } else {
-          ans.img = books.items[i].volumeInfo.imageLinks.thumbnail;
-          bookItem.img = books.items[i].volumeInfo.imageLinks.thumbnail;
-        } 
+      if (checkTitle.length > 28) {
+        ans.dispTitle = checkTitle.join("").substr(0,28)+"...";      
+      } else {
+        ans.dispTitle = books.items[i].volumeInfo.title;
+      }
+      
+      if (books.items[i].volumeInfo.imageLinks == undefined) {
+        ans.img = "/mysterybook.jpg";
+        bookItem.img = "/mysterybook.jpg";
+      } else {
+        ans.img = books.items[i].volumeInfo.imageLinks.thumbnail;
+        bookItem.img = books.items[i].volumeInfo.imageLinks.thumbnail;
+      } 
 
-        ans.authors = books.items[i].volumeInfo.authors;
-        ans.description = books.items[i].volumeInfo.description;
-        ans.id = books.items[i].id;
-        ans.el = i;
+      ans.authors = books.items[i].volumeInfo.authors;
+      ans.description = books.items[i].volumeInfo.description;
+      ans.id = books.items[i].id;
+      ans.el = i;
 
-        bookItem.description = books.items[i].volumeInfo.description;
-        bookItem.book_id = books.items[i].id;
-        bookItem.title = books.items[i].volumeInfo.title;
-        bookItem.rating = books.items[i].volumeInfo.averageRating || "";
-        bookItem.ratingCount = books.items[i].volumeInfo.ratingsCount || "";
-        bookItem.link = books.items[i].accessInfo.webReaderLink;
-        booksObj[i] = bookItem;
+      bookItem.description = books.items[i].volumeInfo.description;
+      bookItem.book_id = books.items[i].id;
+      bookItem.title = books.items[i].volumeInfo.title;
+      bookItem.rating = books.items[i].volumeInfo.averageRating || "";
+      bookItem.ratingCount = books.items[i].volumeInfo.ratingsCount || "";
+      bookItem.link = books.items[i].accessInfo.webReaderLink;
+      booksObj[i] = bookItem;
 
-        bookList.push(ans);
-        counter++;
-      };
+      bookList.push(ans);
+      counter++;
+    };
 
-      var source = $('#book-template').html();
-      var template = Handlebars.compile(source);
-      var html = template({books:bookList});
+    var source = $('#book-template').html();
+    var template = Handlebars.compile(source);
+    var html = template({books:bookList});
 
-      $('.main').html('')
-      $('.main').append(html);
+    $('.main').html('')
+    $('.main').append(html);
   };
 
   API.getAll(getBooks);
@@ -89,21 +97,27 @@ $(document).ready(function() {
   });
 
   $('.main').on('click','.show-book',function(e) {
+    // e.preventDefault();
     var $el = $(this).children().children("input.bookEl").val();
     var book = booksObj[$el];
 
-    $.post("/books", 
-      {
-        book:{
-          title: book.title, 
-          book_id: book.book_id, 
-          rating: book.rating,
-          ratingCount: book.ratingCount,
-          description: book.description,
-          link: book.link,
-          img: book.img
-        }  
-      });
+    console.log("show-book",h,book);
+    $('.bookShowTitle').html(book.title);
+    $('.bookShowImg').html('<img src="' + book.img + '">');
+    $('.bookShowRating').html('Average Rating: ' + book.rating);
+ 
+    // $.post("/books", 
+    //   {
+    //     book:{
+    //       title: book.title, 
+    //       book_id: book.book_id, 
+    //       rating: book.rating,
+    //       ratingCount: book.ratingCount,
+    //       description: book.description,
+    //       link: book.link,
+    //       img: book.img
+    //     }  
+    //   });
   });
 
   $('.search-div').on('submit',function(e) {
@@ -134,10 +148,8 @@ $(document).ready(function() {
   });
 
   $('.homeLink').on('click',function(e) {
-    e.preventDefault();
     console.log("hey");
-    var sPageURL = window.location.search;
-    console.log(sPageURL);
+    API.getAll(getBooks)
     // API.find();
   })
 });
